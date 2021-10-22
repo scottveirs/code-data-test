353b87e09e78e01d47c037609689eb836a927956
Scott Veirs <scott@beamreach.org>
2021-10-21 17:43:23 -0700

    Test of add all with generic commit

--- ts2mp3.sh
+++ ts2mp3.sh
@@ -8,12 +8,20 @@
 # i.e. within aws sync call or in the for loop (delete some .ts segments; rename others)
 
 echo "You provided $# arguments: $1, $2, $3, and $4"
-aws s3 sync s3://streaming-orcasound-net/rpi_$1/hls/$2/ data-input-dir
-for file in data-input-dir/live*; do mv "$file" "${file#live}"; done;
-for i in data-input-dir/*.ts ; do
+aws s3 sync s3://streaming-orcasound-net/rpi_$1/hls/$2/ .
+for file in live*; do mv "$file" "${file#live}"; done;
+for i in *.ts ; do
     mv $i `printf '%04d' ${i%.ts}`.ts
 done
-printf "file '%s'\n" data-input-dir/*.ts > mylist.txt
-ffmpeg -f concat -safe 0 -i mylist.txt -c copy data-output-dir/all.ts
-ffmpeg -i data-output-dir/all.ts -c:v libx264 -c:a copy -bsf:a aac_adtstoasc data-output-dir/output.mp4
-ffmpeg -i data-output-dir/output.mp4 output.mp3
+printf "file '%s'\n" *.ts > mylist.txt
+
+ffmpeg -f concat -safe 0 -i mylist.txt -c copy all.ts
+ffmpeg -i all.ts -c:v libx264 -c:a copy -bsf:a aac_adtstoasc output.mp4
+ffmpeg -i output.mp4 output.mp3
+
+mv *.ts data-input-dir
+mv mylist.txt data-input-dir
+mv live.m3u8 data-input-dir
+rm output.mp4
+mv output.mp3 data-output-dir
+
